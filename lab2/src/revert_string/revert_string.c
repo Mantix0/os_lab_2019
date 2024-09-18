e377428f55e4eee7647886c0f90d15f644757b8d
Mantix <89809041+Mantix0@users.noreply.github.com>
2024-09-18 17:17:09 +0000

    commit

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,23 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
 	// your code here
+
+    int first = 0;
+    int last = strlen(str) - 1;
+    char temp;
+
+    while (first < last) {
+      
+        // Swap characters
+        temp = str[first];
+        str[first] = str[last];
+        str[last] = temp;
+
+        first++;
+        last--;
+    }
 }
 
